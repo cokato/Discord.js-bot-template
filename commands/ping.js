7a01ea7d989968f70576a21eecf4ebc7eb13d179
cokato <135673106+cokato@users.noreply.github.com>
2023-08-28 18:05:55 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     config: {
         name: 'ping',
         description: 'Get ping of the bot',
-        usage: `!ping`,
+        usage: `e!ping`,
     },
     async run (bot,message,args) {
         message.channel.send("My ping is \`" + bot.ws.ping + " ms\`");
