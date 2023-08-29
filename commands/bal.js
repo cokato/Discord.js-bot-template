3c5602332d7229e068b96990e2e3179939812664
cokato <135673106+cokato@users.noreply.github.com>
2023-08-28 18:12:27 -0700

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+    config: {
+        name: 'balance',
+        description: 'See your stats',
+        usage: `e!bal`,
+    },
+    async run (bot,message,args) {
+        message.channel.send("You have");
+    }
+}
